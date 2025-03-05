d985435a406476d33e9c72bcd8e20dceacea5705
a1rzayev <asifrzayev2006@example.com>
2025-03-05 19:27:40 +0400

    Fix expense adding

---
+++ expensetracker/src/main/java/com/expensetracker/App.java
@@ -0,0 +1,37 @@
+package com.expensetracker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Expense Tracker");
+
+        Button btn = new Button("Add Expense");
+        btn.setOnAction(e -> System.out.println("Expense Added"));
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(btn);
+
+        Scene scene = new Scene(layout, 400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
