35a0a3cd132c34fa6716341aa5c7dfde9de21fb0
a1rzayev <asifrzayev2006@example.com>
2025-03-05 13:42:17 +0400

    Create simple expence-tracker app

--- expencetrackerdesktop/src/main/java/com/expensetracker/App.java
+++ expencetrackerdesktop/src/main/java/com/expensetracker/App.java
@@ -1,38 +1,28 @@
 package com.expensetracker;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
-    private static Scene scene;
-
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
-        stage.setScene(scene);
-        stage.show();
-    }
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Expense Tracker");
 
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+        BorderPane root = new BorderPane();
+        root.setCenter(new Text("Welcome to Expense Tracker!"));
+        
+        Scene scene = new Scene(root, 300, 250);
+        scene.getStylesheets().add(getClass().getResource("/application.css").toExternalForm());
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
-
 }
